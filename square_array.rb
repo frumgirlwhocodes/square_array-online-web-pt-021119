6e671666517f68c30c138a5762ecd79930fabf34
lieby weintraub <liebahinda@gmail.com>
2019-02-18 17:10:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
   new_array=[]
- array.each do |number|
-   new_array << number ** 2 
+ array.each do |x|
+   new_array << x**2 
    new_array
     
 end
